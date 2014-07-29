dbcfd4d7e12388e89a23b64d58c7271dc40c5a91
Samir Ahmed <samirahmed2013@gmail.com>
2014-07-28 22:07:36 -0700

    parse AST to query object
    parse the abstract syntax tree into a query object
    update the CFG such that node names can be used to populate wildcards
    appropriately
    
    log results to test.results

--- grammar_test.py
+++ grammar_test.py
@@ -4,6 +4,85 @@ import re
 import sys
 from termcolor import colored
 
+class Query:
+  
+  sender          = None
+  recipients      = None
+  first_text      = None
+  second_text     = None
+  conjunction     = None
+  attachments     = None
+  date            = None
+  date_comparator = None
+  link            = None
+
+  _last_node = None
+
+  def __init__(self, tree, wildcards):
+    self._visit(tree, wildcards)
+  
+  def __str__(self):
+    result = "Sender %s\n" % self.sender
+    result += "Recipients: %s\n" % self.recipients
+    result +=  "First Text: %s\n" % self.first_text
+    result +=  "Second Text: %s\n" % self.second_text
+    result +=  "Conjunction : %s\n" % self.conjunction
+    result +=  "Attachment : %s\n" % self.attachments
+    result +=  "Date : %s\n" % self.date
+    result +=  "Date Comparator : %s\n" % self.date_comparator
+    result +=  "Links : %s\n" % self.link
+    return result
+
+  def _visit(self, tree, wildcards, index = 0 ):
+    
+    if not tree or not (type(tree) == nltk.tree.Tree):
+      return
+
+    if tree.node == "TCSP":    # To Contact Specifier
+      self.recipients = wildcards[index]
+      index += 1
+
+    elif tree.node == "FCSP":  # From Contact Specifier
+      self.sender = wildcards[index]
+      index += 1
+
+    elif tree.node == "FTCSP": # From To Contact Specifier
+      self.sender = wildcards[index]
+      self.recipients = wildcards[index+1]
+      index += 2
+
+    elif tree.node == "TFCSP": # To From Contact Specifier 
+      self.recipients = wildcards[index]
+      self.sender = wildcards[index+1]
+      index += 2
+
+    elif tree.node == "FASP":  # Filename Attachment Specifier
+      self.attachments = wildcards[index]
+      index += 1
+
+    elif tree.node == "STSP":  # Single Text Specifier
+      self.first_text = wildcards[index]
+      index += 1
+
+    elif tree.node == "CTSP":  # Conjunctive Text Specifier
+      self.first_text = wildcards[index]
+      self.second_text = wildcards[index+1]
+      self.conjunction = "and" if tree.leaves()[-2] == "and" else "or"
+      index += 2
+
+    elif tree.node == "LSP":   # Link Specifier
+      self.link = wildcards[index]
+      index += 1
+    
+    elif tree.node == "DSP":   # DateTime Specifier
+      self.date = wildcards[index]
+      self.date_comparator = "after" if tree.leaves()[-2] == "after" else "before"
+      index += 1
+
+    else:
+      for subtree in tree:
+        self._visit(subtree, wildcards, index)
+
 def normalize(terminals, query, debug):
   
   debug.write("\n" + query)
@@ -33,9 +112,11 @@ def parse(parser, terminals, query, debug):
   if not parsetree:
     print colored("ERROR PARSING %s " % query, 'red'),
     debug.write("PARSE ERROR \n")
+    return false
   else:
     sys.stdout.write(colored(u'\u2713', 'green'))
     debug.write(str(parsetree) + "\n")
+    return parsetree, wildcards
 
 lines = open("email.cfg").readlines()
 grammar = '\n'.join(filter(lambda line: not line.startswith("%"),lines))
@@ -50,10 +131,12 @@ with  open("test.result","w") as debug:
   debug.write("Terminals %s" % terminals)
   for line in open('./queries.txt').readlines():
     if line and not line.startswith("#") and len(line.split()) > 0:
-      parse(email_parser, terminals, line, debug)
+      tree, wc = parse(email_parser, terminals, line, debug)
       count += 1
       if count % 50 == 0:
         sys.stdout.write("\n")
       sys.stdout.flush()
+      query = Query(tree, wc)
+      debug.write(str(query) + "\n")
 
 print "\n%d lines parsed" % count
