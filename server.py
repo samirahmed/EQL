fb05ed9718554ff644a977645c9f6e5b06772bb5
Samir Ahmed <samirahmed2013@gmail.com>
2014-07-29 07:40:33 -0700

    update default port

--- server.py
+++ server.py
@@ -22,4 +22,4 @@ def index(query):
   result['result'] = eq.properties()
   return result
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
