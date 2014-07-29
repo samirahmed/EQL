cda46e75395ecc3498bd32d863938671ec4a7efb
Samir Ahmed <samirahmed2013@gmail.com>
2014-07-29 12:33:46 -0700

    add cors

--- server.py
+++ server.py
@@ -4,13 +4,14 @@ import re
 import sys
 import os
 from termcolor import colored
-from bottle import route, run, template
+from bottle import route, run, template, Bottle, request, response
 from grammar_test import AbstractSyntaxTree, EmailQuery, email_parser, terminals
 from faker import Factory
 from datetime import datetime
 
 faker = Factory.create()
 debug = sys.stdout
+app = Bottle()
 
 def fake_contact(times):
   global faker
@@ -22,16 +23,21 @@ def fake_contact(times):
     });
   return contacts
 
-@route('/terminals')
+@app.hook('after_request')
+def enable_cors():
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+@app.route('/terminals')
 def terms():
   return {'terms': list(terminals)}
 
-@route('/fake')
+@app.route('/fake')
 def fake():
   global faker
   text = faker.text()
-  response = {} 
-  result = { "parse_success": True, "duration" : 0.2 }
+  search = {} 
   emails = []
   for times in range(len(faker.text()) % 5):
     emails.append({
@@ -46,11 +52,17 @@ def fake():
     "sent_time" : datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
     "deeplink" : "outlook://125125kj12kj124124125"
     });
-  response["result"] = result
-  response["emails"] = emails
-  return response
+  result = { 
+    "parse_success": True, 
+    "duration" : 0.2, 
+    "count" : len(emails)
+  }
 
-@route('/debug/<query>')
+  search["result"] = result
+  search["emails"] = emails
+  return search
+
+@app.route('/debug/<query>')
 def index(query):
   result = {}
   ast = AbstractSyntaxTree(email_parser, terminals, query, debug)
@@ -59,4 +71,4 @@ def index(query):
   result['result'] = eq.properties()
   return result
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
